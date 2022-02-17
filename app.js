300640655c2563c538a08da8d708bf1694b80408
marco.corallo.1997@gmail.com <marco.corallo.1997@gmail.com>
2022-02-17 17:20:23 +0100

    Creo il ciclo con la generazione delle 6 cards

--- app.js
+++ app.js
@@ -1,50 +1,59 @@
-// const team = [
-//     {
-//       name: ‘Wayne Barnett’,
-//       role: ‘Founder & CEO’,
-//       image: ‘wayne-barnett-founder-ceo.jpg’,
-//     },
-//     {
-//       name: ‘Angela Caroll’,
-//       role: ‘Chief Editor’,
-//       image: ‘angela-caroll-chief-editor.jpg’,
-//     },
-//     {
-//       name: ‘Walter Gordon’,
-//       role: ‘Office Manager’,
-//       image: ‘walter-gordon-office-manager.jpg’,
-//     },
-//     {
-//       name: ‘Angela Lopez’,
-//       role: ‘Social Media Manager’,
-//       image: ‘angela-lopez-social-media-manager.jpg’,
-//     },
-//     {
-//       name: ‘Scott Estrada’,
-//       role: ‘Developer’,
-//       image: ‘scott-estrada-developer.jpg’,
-//     },
-//     {
-//       name: ‘Barbara Ramos’,
-//       role: ‘Graphic Designer’,
-//       image: ‘barbara-ramos-graphic-designer.jpg’,
-//     },
-// ];
-
 // COLLEGO IL "TEAM-CONTAINER"
 const teamContainer = document.querySelector('.team-container');
 console.log(teamContainer);
 
-// CREO L'ELEMENTO ALL'INTERNO DEL "TEAM-CONTAINER"
-const teamCard = teamContainer.innerHTML =
-`<div class="team-card">
+// ARRAY DI OGGETTI PER RIEMPIRE LE CARD
+const team = [
+    {
+      name: 'Wayne Barnett',
+      role: 'Founder & CEO',
+      image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+      name: 'Angela Caroll',
+      role: 'Chief Editor',
+      image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+      name: 'Walter Gordon',
+      role: 'Office Manager',
+      image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+      name: 'Angela Lopez',
+      role: 'Social Media Manager',
+      image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      image: 'scott-estrada-developer.jpg',
+    },
+    {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      image: 'barbara-ramos-graphic-designer.jpg',
+    },
+];
+
+console.log(team);
+
+// CREO CICLO PER GENERARE LE 6 CARD
+for (let i = 0; i < 6; i++) {
+
+    // CREO L'ELEMENTO ALL'INTERNO DEL "TEAM-CONTAINER"
+    const card = teamContainer.innerHTML += `
+    <div class="team-card">
     <div class="card-image">
-        <img src="" alt="">
+    <img src="" alt="">
     </div>
     <div class="card-text">
-      <h3></h3>
-      <p></p>
+    <h3></h3>
+    <p></p>
     </div>
-</div>`;
+    </div>
+    `
 
-console.log(teamCard);
+    console.log(card);
+
+}
